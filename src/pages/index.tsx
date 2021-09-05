83e34f1bdc856764e833f1c978f20770238f19f1
Allan Martins <allanmartins@id.uff.br>
2021-09-05 03:36:41 -0300

    card component for index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
+import {Card} from "../components/Card/Card"
+
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
     <section className={styles.container}>
-      <div className={styles.announce}>
-        
-      </div>
+      <Card/>
     </section>
   )
 }
