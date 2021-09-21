5b1687e3f60bc3ad5997ada725f0451513c40fbf
Allan Martins <allanmartins@id.uff.br>
2021-09-21 19:15:44 -0300

    delete old files

--- petfeliz-frontend/src/components/LoadingScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function LoadingScreen() {
-    return (
-        <div className="loading">
-            <i className='fas fa-spinner fa-spin'></i> Loading ...
-        </div>
-    )
-}
--- petfeliz-frontend/src/components/MessageScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MessageScreen(props) {
-    return (
-        <div>
-            {props.children}
-        </div>
-    )
-}
